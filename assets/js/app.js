e945ea3c44276b46fbd88d5c05cfded1b5f54561
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-02-07 09:46:24 +0530

    js_questions

--- assets/js/app.js
+++
@@ -1,5 +0,0 @@
-let = loop_line.querySelector("#loop_line")
-for(let x=1, y=0;x,y<31;x=x*=2,y++){console.log(x)
-let text=document.createElement("p")
-text.innerHTML=x
-document.querySelector('#loop_line').appendChild(text)}
